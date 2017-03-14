44505bb87ecfee2d0c7f48bff7f6433f1bc3481a
Stanley Cruse <stanley.cruse3@gmail.com>
2017-03-14 11:54:27 -0400

    finished ex3 cruse-select.sql

--- student-exercises/cruse-select.sql
+++ student-exercises/cruse-select.sql
@@ -6,5 +6,14 @@ USE rockStarDay2;
 
 -- SELECT ID, LastName, BirthDate FROM individual WHERE Year(BirthDate) > 1940;
 
-SELECT * FROM individual WHERE ID in (1,3,5,7,19);
+-- SELECT * FROM individual WHERE ID in (1,3,5,7,19);
+
+-- SELECT name FROM band WHERE genre = 'alternative' && isTogether = '0';
+
+-- SELECT ID, name FROM band WHERE ID = '4';
+
+
+
+
+
 
