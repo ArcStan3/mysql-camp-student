6175b1232b58b46d9e1d8819993180ce996235de
Stanley Cruse <stanley.cruse3@gmail.com>
2017-03-14 11:44:45 -0400

    finished ex2 cruse-select.sql

--- student-exercises/cruse-select.sql
+++ student-exercises/cruse-select.sql
@@ -1,3 +1,10 @@
 USE rockStarDay2;
 
-SELECT ID, Name, Genre FROM band;
+-- SELECT * FROM individual WHERE LastName = 'Jennings';
+
+-- SELECT ID, LASTNAME FROM individual WHERE DeceasedDate IS NOT NULL;
+
+-- SELECT ID, LastName, BirthDate FROM individual WHERE Year(BirthDate) > 1940;
+
+SELECT * FROM individual WHERE ID in (1,3,5,7,19);
+
